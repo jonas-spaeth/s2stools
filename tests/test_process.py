c891eddad715f33890fe4ffb9c3ab777da9aa76a
ru35hub <jonas.spaeth@campus.lmu.de>
2021-11-29 14:52:58 +0100

    add validtime

--- tests/test_process.py
+++ tests/test_process.py
@@ -3,6 +3,8 @@ import process
 import os
 import xarray as xr
 
+import utils
+
 
 class TestProcess(TestCase):
 
@@ -11,3 +13,14 @@ class TestProcess(TestCase):
         print(os.listdir("../data"))
         ds = process.open_files(path_pattern=directory + "s2s_ecmwf_uv_20*", max_lt=1)
         self.assertIsInstance(ds, xr.Dataset)
+
+    def test_add_validtime(self):
+        ds = process.open_files(path_pattern="../data/s2s_ecmwf_uv_20*")
+        print(ds)
+        ds_vt = process.add_validtime(ds)
+        self.assertIn("validtime", list(ds_vt.coords))
+        vt_min = ds_vt.validtime.min()
+        hc_year_min = ds.hc_year.min().values
+        reftime_min = ds.reftime.min().values
+        earliest_date = utils.add_years(reftime_min, hc_year_min)
+        self.assertEqual(earliest_date, vt_min)
