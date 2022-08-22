38ff107880c437b0da0698a9a3a42deec81605cc
jonas-spaeth <jonas-spaeth@gmx.de>
2022-08-22 11:31:46 +0200

    squash! upload documentation updated3

--- s2stools/plot/_xarray.py
+++ s2stools/plot/_xarray.py
@@ -64,3 +64,27 @@ def mean_and_error(
     else:
         plot_helper(da.sel({hue: hue_sel}), label=hue_sel.values)
     return ax
+
+
+# def plot_dataarray_map(dataarray, ax=None, **plot_kws):
+#     if ax is None:
+#         plt.figure(figsize=(10, 5))
+# 
+#     p = dataarray.plot.contourf(
+#         subplot_kws=dict(projection=ccrs.PlateCarree()),
+#         transform=ccrs.PlateCarree(),
+#         **plot_kws
+#     )
+# 
+#     p.axes.coastlines()
+#     gl = p.axes.gridlines(
+#         draw_labels=True,
+#         dms=True,
+#         x_inline=False,
+#         y_inline=False,
+#         alpha=0.3,
+#     )
+#     gl.top_labels = False
+#     gl.right_labels = False
+#     return p
+# 
