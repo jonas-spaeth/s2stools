d273714f53677ec52463b7398f6cad5688203710
ru35hub <jonas.spaeth@campus.lmu.de>
2023-07-19 17:49:16 +0200

    add from .x import y

--- s2stools/__init__.py
+++ s2stools/__init__.py
@@ -1,4 +1,6 @@
-# from .basic import zonal_mean, meridional_mean
-# from .zonal_waves import zonal_wave_coeffs
+from .clim import climatology
+from .events import EventComposite, find_ssw
+from .process import s2sparser
+from .plot import spaghetti, xlim_days, xaxis_unit_days, cyclic_xyz
 
 __version__ = "0.3.0"
