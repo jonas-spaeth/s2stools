65533a92a50405ba81a69d2b4a81ed76d136d3ef
jonas-spaeth <jonas-spaeth@gmx.de>
2023-07-21 12:12:27 +0200

    clean comments

--- s2stools/__init__.py
+++ s2stools/__init__.py
@@ -1,9 +1,3 @@
-# from .clim import climatology
-# from .events import EventComposite, find_ssw
-# from .process import s2sparser
-# from .plot import spaghetti, xlim_days, xaxis_unit_days, cyclic_xyz
-
-from . import clim, events, process, plot, indices, utils
-from . import download
+from . import clim, events, process, plot, indices, utils, compute, download
 
 __version__ = "0.3.0"
